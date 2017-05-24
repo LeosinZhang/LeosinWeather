d6a6b517f54c7acf153e7804e9de32d219ffc418
LeosinZhangGoldTelPC <at.zhangxin@foxmail.com>
2017-05-24 17:16:20 +0800

    Add some new file

---
+++ app/src/main/java/leosin/leosinweather/view/activity/SerachCityActivity.java
@@ -0,0 +1,10 @@
+package leosin.leosinweather.view.activity;
+
+/**
+ * Author: LeosinZhang
+ * Time: 2017/5/24 17:13
+ * Describle: for add ViewPager. serach & select city
+ */
+public class SerachCityActivity extends BaseActivity{
+
+}
