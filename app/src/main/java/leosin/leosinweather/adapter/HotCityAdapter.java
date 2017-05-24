60f374655385417382107c9b8ae1b2708b858ffd
LeosinZhang <at.zhangxin@foxmail.com>
2017-05-24 23:40:51 +0800

    add hotCity

---
+++ app/src/main/java/leosin/leosinweather/adapter/HotCityAdapter.java
@@ -0,0 +1,86 @@
+package leosin.leosinweather.adapter;
+
+import android.app.IntentService;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.orhanobut.logger.Logger;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import leosin.leosinweather.R;
+import leosin.leosinweather.view.activity.SearchCityActivity;
+
+/**
+ * Author: LeosinZhang
+ * Time: 2017/5/24 21:42
+ * function:
+ */
+
+public class HotCityAdapter extends RecyclerView.Adapter<HotCityAdapter.ViewHolder> {
+    List<String> cityList;
+    SearchCityActivity mSearchCityActivity;
+
+    public HotCityAdapter(List<String> list, SearchCityActivity searchCityActivity) {
+        this.cityList = list;
+        mSearchCityActivity = searchCityActivity;
+    }
+
+
+    @Override
+    public HotCityAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ViewHolder holder = new ViewHolder(LayoutInflater.from(mSearchCityActivity).inflate(R.layout.layout_hot_city_item, parent, false));
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.tv_hotCity.setText(cityList.get(position));
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tv_layout_hotCity)
+        TextView tv_hotCity;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+
+            //set item view listener
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View itemView) {
+                    //Logger.d("点击到Adapter的第" + getAdapterPosition() + "个item");
+                    int position = getAdapterPosition();
+                    mOnItemClickListener.onItemClick(itemView, position);
+                }
+            });
+
+        }
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return cityList.size();
+    }
+
+    public interface OnItemClickListener
+    {
+        void onItemClick(View view, int position);
+    }
+
+    private OnItemClickListener mOnItemClickListener;
+
+    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener)
+    {
+        this.mOnItemClickListener = mOnItemClickListener;
+    }
+}
--- app/src/main/java/leosin/leosinweather/view/activity/SearchCityActivity.java
+++ app/src/main/java/leosin/leosinweather/view/activity/SearchCityActivity.java
@@ -1,28 +1,48 @@
 package leosin.leosinweather.view.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
+
+import com.orhanobut.logger.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import leosin.leosinweather.R;
+import leosin.leosinweather.adapter.HotCityAdapter;
 
 /**
  * Author: LeosinZhang
  * Time: 2017/5/24 17:13
- * Describle: for add ViewPager. serach & select city
+ * Describe: for add ViewPager. search & select city
  */
-public class SearchCityActivity extends BaseActivity{
-
+public class SearchCityActivity extends BaseActivity {
+    private HotCityAdapter hotCityAdapter;
+    private SearchCityActivity searchCityActivity = this;
+    private Context context;
     @BindView(R.id.recyclerView_activity_addViewPager_hotCity)
     RecyclerView mRecyclerView;
     @BindView(R.id.edit_activity_addViewPager_search)
     EditText editSearchCity;
-    @BindView(R.id.img_activity_addViewPager_clear)
-    ImageView imgClearText;
+
+    ArrayList<String> list = new ArrayList<String>() {
+        {
+            add("北京"); add("成都"); add("重庆"); add("广州");
+            add("杭州"); add("南京"); add("上海"); add("深圳");
+            add("天津"); add("武汉"); add("西安"); add("郑州");
+            add("昆明"); add("沈阳"); add("哈尔滨"); add("长沙");
+            add("石家庄"); add("苏州"); add("无锡"); add("济南");
+            add("大连"); add("厦门"); add("南昌"); add("长春");
+        }
+    };
 
 
     @Override
@@ -30,7 +50,9 @@ public class SearchCityActivity extends BaseActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_viewpager);
         ButterKnife.bind(this);
+        context = this;
         clearText();
+        initRecyclerView();
     }
 
 
@@ -40,8 +62,26 @@ public class SearchCityActivity extends BaseActivity{
     }
 
     @OnClick(R.id.img_activity_addViewPager_clear)
-    public void clearText(){
+    public void clearText() {
         editSearchCity.setText("");
+    }
+
+
+    /**
+     * 初始化RecyclerView
+     */
+    private void initRecyclerView() {
+        hotCityAdapter = new HotCityAdapter(list,searchCityActivity);
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL));
+        mRecyclerView.setAdapter(hotCityAdapter);
+        hotCityAdapter.setOnItemClickListener(new HotCityAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Logger.d("点击位置结果 ->" + position );
+
+            }
+
+        });
     }
 
 }
